b9a75fe24c41b08fe9b58fe56f856fd81c6e4601
monirakibul <monirakibul@gmail.com>
2022-04-05 23:13:56 +0600

    readme edited

--- src/components/AreaChart/AreaChart.js
+++ src/components/AreaChart/AreaChart.js
@@ -64,7 +64,7 @@ export default function MyAreaChart() {
                 }}
             >
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
+                <XAxis dataKey="month" />
                 <YAxis />
                 <Tooltip />
                 <Area type="monotone" dataKey="investment" stroke="#8884d8" fill="#8884d8" />
